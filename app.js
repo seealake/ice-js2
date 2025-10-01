c0a170304a2818df9b6404bf63cc7edbaca8718e
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-10-01 16:13:27 -0500

    better still

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ function invest() {
 }
 
 console.log(invest());
+
+/*************************************
+Ignore what is below for now...     */
+
+function displayInvestment() {
+  const investment = invest();
+  alert("The investment will be worth $" + investment);
+}
