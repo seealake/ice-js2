a2b7f667caf796db5642904d529b6af4052e9f6f
Yulong Wu <wu.yulong@wustl.edu>
2025-10-01 17:43:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ console.log("app.js loaded");
 
 function invest() {
   // TODO - your investment calculation!
+  const principal = getPrincipal();
+  const returnPerc = getReturnPerc();
+  const numOfYears = getNumOfYears();
+  const total = principal * Math.pow((1 + returnPerc), numOfYears);
+  return total.toFixed(2);
 }
 
 console.log(invest());
