d91bb40f119ca0d3bd4f81092b8579d902c22b56
Yulong Wu <wuyulong200009@gmail.com>
2025-11-16 02:45:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function invest() {
   // TODO - your investment calculation!
   principal = 100;
   interestRate = 0.05;
+  time = 1
+  return principal * (1 + interestRate) ** time;
 }
 
 console.log(invest());
@@ -15,3 +17,4 @@ function displayInvestment() {
   const investment = invest();
   alert("The investment will be worth $" + investment);
 }
+invest();
